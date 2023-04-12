d309341853aa2a6f0f445735dfa1892e056f590b
baizx98 <baizx98@gmail.com>
2023-04-12 15:26:52 +0800

    change formatter

--- profiler/tool.py
+++ profiler/tool.py
@@ -14,7 +14,9 @@ import matplotlib.pyplot as plt
 import matplotlib
 
 
-def reindex_nid_by_hot_metric(hot_metric: Union[torch.Tensor, List[torch.Tensor]]) -> Union[torch.Tensor, List[torch.Tensor]]:
+def reindex_nid_by_hot_metric(
+    hot_metric: Union[torch.Tensor, List[torch.Tensor]]
+) -> Union[torch.Tensor, List[torch.Tensor]]:
     if isinstance(hot_metric, torch.Tensor):
         _, pre_order = torch.sort(hot_metric)
         return pre_order
@@ -24,15 +26,16 @@ def reindex_nid_by_hot_metric(hot_metric: Union[torch.Tensor, List[torch.Tensor]
             _, temp = torch.sort(li, descending=True)
             prev_order_list.append(temp)
         return prev_order_list
-    logging.info('reindex done')
+    logging.info("reindex done")
 
 
 def get_dataset_save_path(dataset_name: str) -> str:
-    return osp.join(osp.dirname(
-        osp.realpath(__file__)), '..', 'data', dataset_name)
+    return osp.join(osp.dirname(osp.realpath(__file__)), "..", "data", dataset_name)
 
 
-def get_profiler_data_save_path(file_name: str, profiler_data_path='profiler/data/') -> str:
+def get_profiler_data_save_path(
+    file_name: str, profiler_data_path="profiler/data/"
+) -> str:
     """获取分析结果的保存路径
     Args:
         file_name (str): 文件名
