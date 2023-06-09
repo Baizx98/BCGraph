ae5836a44a8cb8538c66345048209fc77400b7dc
baizx98 <baizx98@gmail.com>
2023-06-09 15:17:15 +0800

    train nodes partition algorithm 1.0,Load imbalance

--- profiler/patititon_test.py
+++ profiler/patititon_test.py
@@ -80,55 +80,122 @@ def train_idx_partition(
     train_idx: np.ndarray,
     partition_num: int,
 ):
+    # TODO 设置随机数种子
+    ...
+    ### 注意csrtopo中的为tensor，下面代码中需要的是numpy array！！！
     # 创建与GPU数量相同的列表来保存为每个GPU分配的训练节点ID
-    pa_list_dic = {i: [] for i in partition_num}
+    pa_list_dic = {i: [] for i in range(partition_num)}
 
     train_idx_num = train_idx.shape[0]
+    # 全图节点中标记为True的节点时训练节点
+    train_mask: np.ndarray = np.zeros(csrtopo.node_count, dtype=bool)
+    train_mask[train_idx] = True
     # accessed mask,用来标记训练节点是否被访问
-    train_visited = [False] * train_idx_num
-    full_visited = [False] * csrtopo.node_count
+    train_visited: np.ndarray = np.zeros(train_idx_num, dtype=bool)
+    full_visited: np.ndarray = np.zeros(csrtopo.node_count, dtype=bool)
 
     train_visited_count = 0
-
-    # 随机选取partition_num个起始节点,并加入队列作为初始值
+    train_index_dic = {id: index for index, id in enumerate(train_idx)}
+    # print("dic:", train_index_dic)
+    # print(train_index_dic[245])
+    # 从训练集中（或者全图？）随机选取partition_num个起始节点,并加入队列作为初始值
     start_idx = random.sample(range(train_idx.shape[0]), partition_num)
-    print(start_idx)
+    print("seed index:", start_idx)
     start_idx = train_idx[start_idx]
-    print(start_idx)
+    print("seed train idx:", start_idx)
 
-    # 创建各个分区的当前和下一轮的队列 open-closed表
-    # now_que中初始化为种子节点
+    # 创建各个分区的当前和下一轮的队列 (或者open-closed表？)
     bfs_queues = {
         i: {"now_que": queue.Queue(), "next_que": queue.Queue()}
         for i in range(partition_num)
     }
-
+    # now_que中将种子节点入队完成初始化
+    for i, id in enumerate(start_idx):
+        print("seed id type:", type(id))
+        bfs_queues[i]["now_que"].put(id)
+    # 创建邻居节点set，用来保存每层遍历中节点的所有邻居节点（并去重）
+    neighbors_to_next = set()
+    # 对所有分区分层地进行循环分配，直到所有的训练节点都被分配完毕
+    # 最终遍历多少层，由该while循环决定
+    layer_num = 0
+    start = time.time()
     while train_visited_count < train_idx_num:
-        ...
+        print("layer num:", layer_num)
         # 对pa_list中子list按照元素个数进行升序排序
         # sort(pa_list,sub_li_ele_num)
         # 得到排序后的列表 例如[4,2,1,3]
-        pa_list_index_orderd = []
+        # 根据为每个GPU分配的训练节点数量对pa_list进行升序排序并返回训练集分区id
+        # 这样做使训练节点最少的分区能优先被分配新的训练节点
+        pa_list_index_orderd = sorted(pa_list_dic, key=lambda x: len(pa_list_dic[x]))
         for pa_id in pa_list_index_orderd:
-            # 遍历队列中的节点，判断是否为train，并设置visited信息
-            for nid in bfs_queues[pa_id]["now_que"]:  # 队列可以for循环吗？
-                # TODO 先将当前遍历到的层中的节点的邻居节点（也就是下一层的节点）加入到下一层的队列当中
-                ...
-                #
+            # 维护一个数据结构，用来存放下面的一层循环中访问节点的邻居节点（可以有重复或者直接去重）
+            # 使用set是乱序的，无重复的，要注意这里的顺序问题
+            # TODO 注意这里的set创建和清空的方式，是在每次循环开始前创建一个新的set，还是在前面创建set，在这里清空set
+            neighbors_to_next.clear()
+            # 首先遍历now queue中所有节点，每次迭代时判断是否为train，并设置visited信息
+            # 然后将当前节点的邻居添加到next queue中（是否添加随机性？）
+            # 如下是一个分区的一层的遍历
+            while not bfs_queues[pa_id]["now_que"].empty():
+                print("layer:{},pa id:{}".format(layer_num, pa_id))
+                nid = bfs_queues[pa_id]["now_que"].get()
+                # 找到nid的所有邻居节点，从csr格式中获取,将tensor转为numpy
+                neighbor_of_nid = (
+                    csrtopo.indices[csrtopo.indptr[nid] : csrtopo.indptr[nid + 1]]
+                ).numpy()
+                # 无论节点是否被访问过，都加入下一层队列中，也就是多个遍历碰撞后继续重叠遍历
+                # TODO 此处有bug，每层遍历时，对当前层也就是now que中的每个节点，都将其所有邻居节点放入队列，
+                # 但是每个节点的邻居节点可能是存在重复的，应该在每层去重后再添加到下一层的队列
+                # TODO 可以考虑使用update方法，直接将可迭代的列表添加至set
+                # 此处添加下一层访问的节点时并没有考虑多个源遍历发生碰撞的情况，而是默认碰撞后继续重叠遍历
+                # 可以考虑为碰撞的节点添加随机性，也要考虑添加了随机性后算法是否收敛的问题
+                for neighbor in neighbor_of_nid:
+                    # bfs_queues[pa_id]["next_que"].put(neighbor)  # 此处有问题，大量重复节点
+                    neighbors_to_next.add(neighbor)
+                # 处理当前节点的全局访问mask 这一块代码不是必须的
                 if full_visited[nid] == True:
                     continue
-                full_visited[nid] = True
-                if nid in train_idx:
-                    if train_visited[nid] == True:
+                else:
+                    full_visited[nid] = True
+                # 处理当前节点的训练集访问mask
+                if train_mask[nid]:  # TODO
+                    if train_visited[train_index_dic[nid]] == True:
                         continue
+                    # 将训练节点加入到分区列表中 最终结果
+                    # TODO 在最后一层遍历时，可能只完成部分分区的遍历就划分了所有的训练节点，在现有算法的条件下
+                    # 最后一层中未循环的分区节点数较少，分区间负载不均衡
+                    # 一个分区中添加一个或若干个训练节点时，就应该暂停该分区当前层的遍历，开始进行下一个分区
+                    # 这样就保证了训练节点少的分区也有机会分到节点
+                    # 本质上来说，是通过对层遍历的细粒度划分来实现分区间训练节点的负载均衡
+                    ...
                     pa_list_dic[pa_id].append(nid)
-                    train_visited[nid] = True
+                    train_visited[train_index_dic[nid]] = True
                     train_visited_count += 1
-            # 并且将每个节点的邻居节点添加到下一轮队列中
-    ...
+            # 将上面的一层循环中访问节点的所有邻居节点去重后加入到next que中
+            for neighbor in neighbors_to_next:
+                bfs_queues[pa_id]["next_que"].put(neighbor)
+            # 以上一层遍历结束，下面更新now队列和next队列的内容
+            # TODO 添加判断条件 上一层遍历结束的flag是该分区的now que为空，只有now que为空时才更新下一层队列到当前队列
+            bfs_queues[pa_id]["now_que"], bfs_queues[pa_id]["next_que"] = (
+                bfs_queues[pa_id]["next_que"],
+                bfs_queues[pa_id]["now_que"],
+            )
+        layer_num += 1
+
+    end = time.time()
+    print("time:", end - start)
+    return pa_list_dic
+
+
+def train_partition_test():
+    csrtopo = quiver.CSRTopo(edge_index=edge_index)
+    res = train_idx_partition(csrtopo=csrtopo, train_idx=idx, partition_num=4)
+    for key, value in res.items():
+        print("pa id:{},train node num:{}".format(key, len(value)))
+    np.save("/home8t/bzx/padata/reddit/pa_li_dic.npy", res)
 
 
 if __name__ == "__main__":
     # demo_test()
     # reddit_test()
-    train_idx_partition(1, train_idx=idx, partition_num=4)
+    # train_idx_partition(1, train_idx=idx, partition_num=4)
+    train_partition_test()
